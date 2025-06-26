8674b221db97cbb47d40e6298f391efb168ab2a2
unknown <abirawlins8@gail.com>
2025-06-27 02:58:17 +1200

    Updated styling, such as overall theme and colours. This is still in progress though.

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,11 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
   const navbar = document.getElementById('base-header-container');
-  
+  const textNavBar = document.querySelectorAll('.header-list-text-main');
+  const signUpButton = document.querySelector('.header-list-text-sign');
+
   window.addEventListener('scroll', function() {
     if (window.scrollY > 50) {
+      textNavBar.forEach((text)=>{
+        text.classList.add('scroll-state');
+      })
       navbar.classList.add('scroll-state');
+      signUpButton.classList.add('scroll-state');
+
     } else {
+      textNavBar.forEach((text)=>{
+        text.classList.remove('scroll-state');
+      })
       navbar.classList.remove('scroll-state');
+      signUpButton.classList.remove('scroll-state');
+
     }
   })});
 
